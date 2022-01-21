eb2353f3dd2eb214082fce1d8c59a0fe7d6338ec
浪鱼 <wenjieyu.ywj@alibaba-inc.com>
2022-01-21 16:21:29 +0800

    feat: 添加FalttenArray

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+/**
+ * 可配置的Partial
+ * 传入的联合类型key为可选，其他为必选
+ */
 type PartialOptional<T, K extends keyof T> = Omit<T, K> & {[P in K]?: T[P]}
+
+/*
+推导数组所有元素的类型
+ */
+type FalttenArray<T> = T extends Array<infer P>  ? P : never;
+
+type Fa1 = FalttenArray<[1, 2, '3']>
+
+type Fa2 = FalttenArray<[number, number, string]>
+
